b5e5791e3bd918ae29f77170a30ac13e9e1e3052
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-12-03 04:43:17 -0500

    test

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,1 @@
-export default ({ env }) => ({
-  email: {
-    config: {
-      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
-      },
-      settings: {
-        defaultFrom: "20191544@aloe.ulima.edu.pe",
-        defaultReplyTo: "20191544@aloe.ulima.edu.pe",
-        testAddress: "20191544@aloe.ulima.edu.pe",
-      },
-    },
-  },
-});
+export default () => ({});
